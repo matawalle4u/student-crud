3b89e97bbc2371ffdd08c8a3d8a8a8af100e0f0a
Adam Mustapha <matawalle4u@gmail.com>
2023-05-13 12:01:21 +0100

    Implemented CourseRepository to be injected into Course Controller

--- students-crud/app/Repositories/CourseRepository.php
+++ students-crud/app/Repositories/CourseRepository.php
@@ -9,23 +9,43 @@
 
     class CourseRepository {
 
-        public function create() {
-
+        public function create(string $name, string $code): Model|Course {
+            return Course::query()->create([
+                'name' => $name,
+                'code' => $code,
+            ]);
         }
+
         public function findAll(): Collection{
+            return Course::all();
+        }
+        
+        public function findById(int $id): Model|Course|null{
+            return Course::query()->find($id);
+        }
+
+        public function findRequiredById(int $id): Model|Course{
+
+            $course = $this->findById($id);
+            if (null == $course) {
+                throw new ModelNotFoundException('Such course does not exists');
+            }
+            return $course;
+        }
+
+        public function update(int $id, string $name, string $code): Model|Course{
+
+            $course = $this->findRequiredById($id);
+            $course->update([
+                'name' => $name,
+                'code' => $code, 
+            ]);
+            return $course;
+        }
+
+        public function delete(int $id): void{
             
-        }
-        public function findById(int $id){
-
-        }
-        public function findRequiredById(int $id){
-
-        }
-        public function update(int $id, string $name, string $code){
-
-        }
-
-        public function delete(int $id){
-            
+            $course = $this->findRequiredById($id);
+            $course->delete();
         }
     }
