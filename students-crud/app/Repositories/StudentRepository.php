fca1239825d98e6cd762dd2905b77349ae878170
Adam Mustapha <matawalle4u@gmail.com>
2023-05-13 11:48:59 +0100

    Implemented StudentRepository

--- students-crud/app/Repositories/StudentRepository.php
+++ students-crud/app/Repositories/StudentRepository.php
@@ -8,23 +8,51 @@
 
     class StudentRepository {
 
-        public function create() {
-
+        public function create(string $firstName, string $lastName, string $regNo):Model|Student {
+            return Student::query()->create([
+                    'first_name' => $firstName,
+                    'last_name' => $lastName,
+                    'reg_no' => $regNo
+                ]);
         }
+
         public function findAll(): Collection{
+            return Student::all();
+        }
+
+        public function findById(int $id):Model|Student|null{
+
+            return Student::query()->find($id);
+        }
+
+        public function findRequiredById(int $id): Model|Student {
+
+            $student = $this->findById($id);
+            if (null == $student) {
+                throw new ModelNotFoundException('Such student does not exists');
+            }
+
+            return $student;
+        }
+
+        public function update(int $id, string $firstName, string $lastName, string $regNo):Model|Student{
+
+            $student = $this->findRequiredById($id);
+
+            $student->update([
+            'first_name' => $firstName,
+            'last_name' => $lastName,
+            'reg_no' => $regNo
+            ]);
+
+            return $student;
+        }
+
+        public function delete(int $id):int{
             
-        }
-        public function findById(int $id){
+            $student = $this->findRequiredById($id);
+            $student->delete();
 
-        }
-        public function findRequiredById(int $id){
-
-        }
-        public function update(int $id, string $firstName, string $lastName, string $regNo){
-
-        }
-
-        public function delete(int $id){
-            
+            return $student['id'];
         }
     }
