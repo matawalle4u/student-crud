a0575c24751a7ea6c211cd365d1c06d798e13212
Adam Mustapha <matawalle4u@gmail.com>
2023-05-13 12:30:09 +0100

    Implemented StudentController

--- students-crud/app/Http/Controllers/StudentController.php
+++ students-crud/app/Http/Controllers/StudentController.php
@@ -20,4 +20,85 @@ class StudentController extends Controller
     )
     {
     }
+
+    public function index(): View
+    {
+        $students = $this->repository->findAll();
+        return view('students.index', [
+            'students' => $students,
+        ]);
+    }
+
+    public function create(): View
+    {
+        return \view('students.create');
+    }
+
+    public function edit(int $id): View
+    {
+        $student = $this->repository->findById($id);
+        return \view('students.edit', [
+            'student' => $student,
+        ]);
+    }
+
+    public function store(StudentPostRequest $request): RedirectResponse
+    {
+        $student = $this->repository->create(
+            firstName: $request->validated('first_name'),
+            lastName: $request->validated('last_name'),
+            regNo: $request->validated('reg_no')
+        );
+
+        return redirect(sprintf('/students/%d', $student['id']));
+    }
+
+    public function update(int $id, StudentPutRequest $request): RedirectResponse
+    {
+        $student = $this->repository->update(
+            id: $id,
+            firstName: $request->validated('first_name'),
+            lastName: $request->validated('last_name'),
+            regNo: $request->validated('reg_no')
+        );
+
+        return redirect(sprintf('/students/%d', $student['id']));
+    }
+
+    public function show(int $id): View
+    {
+        $student = $this->repository->findById($id);
+        $student_courses = $this->studentCourseRepository->listAll($id);
+        $courses = $this->courseRepository->listAddable($id);
+
+        return \view('students.show', [
+            'student' => $student,
+            'student_courses' => $student_courses,
+            'courses' => $courses,
+        ]);
+    }
+
+    public function destroy(int $id): RedirectResponse
+    {
+         $this->repository->delete($id);
+        return redirect('/students');
+    }
+
+    public function addCourse(int $id, StudentCoursePostRequest $request): RedirectResponse
+    {
+        $this->studentCourseRepository->add(
+            studentId: $id,
+            courseId: $request->validated('course_id')
+        );
+
+        return redirect(sprintf('/students/%d', $id));
+    }
+
+    public function removeCourse(int $studentCourseId): RedirectResponse
+    {
+        $studentId = $this->studentCourseRepository->remove($studentCourseId);
+        return redirect(sprintf('/students/%d', $studentId));
+    }
+
+
 }
