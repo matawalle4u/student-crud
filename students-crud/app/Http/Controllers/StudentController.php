23e91f6ef9829b218d6fe5c30e221c1bfc930e66
Adam Mustapha <matawalle4u@gmail.com>
2023-05-13 12:15:37 +0100

    Injected Relevant Repositories implemented in App\Repositories

--- students-crud/app/Http/Controllers/StudentController.php
+++ students-crud/app/Http/Controllers/StudentController.php
@@ -4,7 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Repositories\CourseRepository;
+use App\Repositories\StudentCourseRepository;
+use App\Repositories\StudentRepository;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+
 class StudentController extends Controller
 {
     //
+    public function __construct(
+        private readonly StudentRepository $repository,
+        private readonly StudentCourseRepository $studentCourseRepository,
+        private readonly CourseRepository $courseRepository,
+    )
+    {
+    }
 }
