d687b6b96aa5e9479485906454a4667ca78a153e
Adam Mustapha <matawalle4u@gmail.com>
2023-05-14 21:27:50 +0100

    altered cypress config.js

---
+++ students-crud/cypress.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'cypress'
+import a from './tests/cypress/plugins/index.js'
+
+
+export default defineConfig({
+    chromeWebSecurity: false,
+    retries: 2,
+    defaultCommandTimeout: 5000,
+    watchForFileChanges: false,
+    videosFolder: 'tests/cypress/videos',
+    screenshotsFolder: 'tests/cypress/screenshots',
+    fixturesFolder: 'tests/cypress/fixture',
+    e2e: {
+        setupNodeEvents(on, config) {
+            return a(on, config)
+            
+        },
+        baseUrl: 'http://localhost',
+        specPattern: 'tests/cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
+        supportFile: 'tests/cypress/support/index.js',
+    },
+})
