f6a0a9111328779955b42ea7f9b220ba40b21a43
Adam Mustapha <matawalle4u@gmail.com>
2023-05-17 11:30:04 +0100

    Implemented course deletion testing with cypress

---
+++ students-crud/tests/cypress/integration/coursedelete.cy.js
@@ -0,0 +1,17 @@
+describe('Deleting course', ()=>{
+    
+    it("deletes courses", () => {
+   
+        cy.visit("/courses");
+        /* 
+          The first is the home anchor, the second is the create
+          To get the first list of students use the thirst anchor .eq(2)
+        */
+        cy.get('a').eq(2)
+        .click()
+  
+       cy.get('a#delete_course').click()
+        
+      });
+  
+  });
